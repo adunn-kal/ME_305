85c9bcd1a9fb1fb0b1a33e757876460982591c24
akdunn <akdunn@calpoly.edu>
2022-02-03 10:55:25 -0800

    Created a motor driver script

---
+++ lab0x03/drv8847.py
@@ -0,0 +1,45 @@
+from motor import Motor
+from pyb import Pin, Timer, ExtInt
+
+
+class DRV8847:
+    '''!
+    '''
+    def __init__(self, sleepPin, faultPin, timer):
+        self.timer = Timer(timer, freq = 20000)
+        self.sleepPin = Pin(sleepPin, mode=Pin.OUT_PP)
+        self.faultPin = faultPin
+        
+    def enable(self):
+        self.sleepPin.high()
+    
+    def disable(self):
+        self.sleepPin.low()
+        
+    def fault_cb(self, IRQ_src):
+        self.disable()
+    
+    def makeMotor(self, IN1_pin, IN2_pin, ch1, ch2):
+        motor = Motor(self.timer, IN1_pin, IN2_pin, ch1, ch2)
+        return motor
+
+        
+if __name__ == '__main__':
+    while True:
+        try:
+            myDriver = DRV8847(Pin.cpu.A15, Pin.cpu.B2, 3)
+            
+            #faultInt = ExtInt(Pin.cpu.B2, mode=ExtInt.IRQ_RISING, pull=Pin.PULL_NONE, callback=myDriver.fault_cb)
+            
+            motor_1 = myDriver.makeMotor(Pin.cpu.B4, Pin.cpu.B5, 1, 2)
+            motor_2 = myDriver.makeMotor(Pin.cpu.B0, Pin.cpu.B1, 3, 4)
+
+            myDriver.enable()
+            
+            motor_1.set_duty(50)
+            motor_2.set_duty(-70)
+            
+        except KeyboardInterrupt:
+            myDriver.disable()
+            print('Disabling motors')
+            break
--- lab0x03/motor.py
+++ lab0x03/motor.py
@@ -2,13 +2,13 @@ from pyb import Pin, Timer
 
 class Motor:
     
-    def __init__(self, PWM_time, IN1_pin, IN2_pin):
+    def __init__(self, PWM_time, IN1_pin, IN2_pin, ch1, ch2):
         self.forwardPin = IN1_pin
         self.backPin = IN2_pin
         
         
-        self.PWMforward = PWM_time.channel(1, Timer.PWM_INVERTED, pin=self.forwardPin)
-        self.PWMback = PWM_time.channel(2, Timer.PWM_INVERTED, pin=self.backPin)
+        self.PWMforward = PWM_time.channel(ch1, Timer.PWM_INVERTED, pin=self.forwardPin)
+        self.PWMback = PWM_time.channel(ch2, Timer.PWM_INVERTED, pin=self.backPin)
                 
     def set_duty(self, duty):
         
@@ -31,12 +31,14 @@ if __name__ == '__main__':
          #   IN1_pin = Pin(Pin.cpu.B4, mode=Pin.OUT_PP)
          #  IN2_pin = Pin(Pin.cpu.B5, mode=Pin.OUT_PP)
             
-            motor_1 = Motor(PWM_time, Pin.cpu.B4, Pin.cpu.B5)
+            motor_1 = Motor(PWM_time, Pin.cpu.B4, Pin.cpu.B5, 1, 2)
+            motor_2 = Motor(PWM_time, Pin.cpu.B0, Pin.cpu.B1, 3, 4)
             
             nSLEEP = Pin(Pin.cpu.A15, mode=Pin.OUT_PP)
             nSLEEP.high()
             
-            motor_1.set_duty(80)
+            motor_1.set_duty(0)
+            motor_2.set_duty(-70)
             
         except KeyboardInterrupt:
             nSLEEP.low()
@@ -44,4 +46,3 @@ if __name__ == '__main__':
             break
             
     
-        
